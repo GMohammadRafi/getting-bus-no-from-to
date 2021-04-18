15c7c8afc8de5828e5dfe8f44d51533a11c74793
G Mohammad Rafi <shafi20014@gmail.com>
2021-04-18 12:02:12 +0530

    fixed all errors in getting bus stops

--- main.py
+++ main.py
@@ -9,49 +9,16 @@ app = Flask('__name__')
 user_need_details = {}
 
 
-def check_exists_by_selector(css_selector, driver):
-    try:
-        driver.find_element_by_css_selector(css_selector)
-    except NoSuchElementException:
-        return False
-    return True
-
-
 def get_url_setting_to_loc(from_loc_lat, from_loc_long, to_loc_lat, to_loc_long):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN") or "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH") or "chromedriver.exe", options=chrome_options)
-    driver.get(f"https://www.google.com/maps/@{from_loc_lat},{from_loc_long},15z")
-    try:
-        driver.find_element_by_css_selector("#gs_lc50 input").send_keys(f"{to_loc_lat},{to_loc_long}")
-        driver.find_element_by_css_selector(".searchbox-directions-container button").click()
-        set_from_loc(from_loc_lat, from_loc_long, driver)
-    except NoSuchElementException:
-        driver.close()
-        get_url_setting_to_loc(from_loc_lat, from_loc_long, to_loc_lat, to_loc_long)
-
-
-def set_from_loc(from_loc_lat, from_loc_long, driver):
-    try:
-        time.sleep(2)
-        driver.find_element_by_css_selector(".gstl_51 .sbib_b input").send_keys(f"{from_loc_lat},{from_loc_long}")
-        driver.find_elements_by_css_selector(".adjusted-to-decreased-spacing div")[4].find_element_by_css_selector(
-            "button").click()
-        setting_bus_as_medium(driver)
-    except NoSuchElementException:
-        set_from_loc(from_loc_lat, from_loc_long, driver)
-
-
-def setting_bus_as_medium(driver):
-    try:
-        time.sleep(2)
-        driver.find_element_by_css_selector(".section-directions-trip-description button").click()
-        get_user_details_from_website(driver)
-    except NoSuchElementException:
-        setting_bus_as_medium(driver)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH") or "chromedriver.exe", chrome_options=chrome_options)
+    driver.get(
+        f"https://www.google.com/maps/dir/'{from_loc_lat},{from_loc_long}'/'{to_loc_lat},{to_loc_long}'/@13.1604339,77.6366039,15z/am=t/data=!4m10!4m9!1m3!2m2!1d77.6366039!2d13.1604339!1m3!2m2!1d{to_loc_long}!2d{to_loc_lat}!3e3")
+    get_user_details_from_website(driver)
 
 
 def get_user_details_from_website(driver):
@@ -69,39 +36,39 @@ def get_user_details_from_website(driver):
         user_need_details["toatal_time"] = str(
             driver.find_element_by_css_selector(".section-layout-root .section-trip-summary h1").text)
         details = driver.find_element_by_css_selector(".section-layout-root .section-trip-details")
+        bus_start_timings = details.find_elements_by_css_selector(".transit-stop .directions-mode-group-departure-time")
         locations = details.find_elements_by_css_selector(".transit-stop-details h2")
-        bus_start_timings = details.find_elements_by_css_selector(
-            ".transit-stop .directions-mode-group-departure-time")
+        timings_no_of_stop = details.find_elements_by_css_selector(".transit-step-details")
+        bus_no = details.find_elements_by_css_selector(".renderable-component-text-box-content")
+
         for i in range(len(bus_start_timings)):
             if bus_start_timings[i].text:
                 temp.append({"starting_bus_timing": bus_start_timings[i].text})
-        c = 0
-        if len(locations) == len(temp) * 2:
-            for i in range(0, len(locations), 2):
-                temp[c]["starting_bus_stop"] = locations[i].text
-                temp[c]["end_bus_stop"] = locations[i + 1].text
-                c += 1
-        else:
-            for i in range(0, len(locations) - 1):
-                temp[i]["starting_bus_stop"] = locations[i].text
-                temp[i]["end_bus_stop"] = locations[i + 1].text
-        # bus_end_timings = details.find_elements_by_css_selector(".transit-stop .directions-mode-group-arrival-time")
-        # add_all(bus_end_timings, "ending_bus_timing")
-        bus_no = details.find_elements_by_css_selector(".renderable-component-text-box-content")
-        add_all(bus_no, "bus_no")
-        timings_no_of_stop = details.find_elements_by_css_selector(".transit-step-details")
-        c = 0
+
+        test = []
         for i in range(len(timings_no_of_stop)):
-            if timings_no_of_stop[i].text and not str(timings_no_of_stop[i].text).startswith("About"):
-                temp[c]["timings_and_no_of_stop"] = timings_no_of_stop[i].text
+            if timings_no_of_stop[i].text:
+                test.append(timings_no_of_stop[i].text)
+        if str(test[0]).startswith("About"):
+            test.pop(0)
+        if str(test[-1]).startswith("About"):
+            test.pop(-1)
+        c = 0
+        i = 0
+        for t in test:
+            if str(t).startswith("About"):
                 c += 1
+            else:
+                temp[i]["starting_bus_stop"] = locations[c].text
+                temp[i]["end_bus_stop"] = locations[c + 1].text
+                temp[i]["timings_and_no_of_stop"] = t
+                c += 1
+                i += 1
+        add_all(bus_no, "bus_no")
         user_need_details["bus_details"] = temp
         getting_iframe(driver)
     except NoSuchElementException:
-        if check_exists_by_selector(".section-directions-trip-description button"):
-            setting_bus_as_medium(driver)
-        else:
-            get_user_details_from_website(driver)
+        get_user_details_from_website(driver)
 
 
 def getting_iframe(driver):
@@ -130,10 +97,10 @@ def getting_iframe(driver):
 def get_bus_no_timings(from_loc_lat, from_loc_long, to_loc_lat, to_loc_long):
     try:
         get_url_setting_to_loc(from_loc_lat=from_loc_lat,
-                               from_loc_long=from_loc_long,
-                               to_loc_lat=to_loc_lat,
-                               to_loc_long=to_loc_long
-                               )
+                           from_loc_long=from_loc_long,
+                           to_loc_lat=to_loc_lat,
+                           to_loc_long=to_loc_long
+                           )
     except:
         return {
             "error": 300,
